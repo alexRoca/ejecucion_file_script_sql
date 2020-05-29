6575a4a152136c25fbbb64b873fcfc35e0b945db
aroca <alexander.roca@actecperu.com>
2020-05-28 21:16:17 -0500

    2da validacion

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-		mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
+		
 		//Se puede usar "mysqli_connect" o "new mysqli", en ambos casos se cponecta a la base de datos usando SQLI
 		//que permite realizar multiples script en un solo envio.
 		
@@ -7,8 +7,14 @@
 		
 		$estado=0;
 		
-		try {
+		//la siguiente linea es para poder capturar algun fallo con la conexion de la base de datos
+		mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
 		
+		try {
+			
+			//el @ es para relizar la conexion de forma obligatoria
+			//con ello se puede hacer la validacion con if, si la variable es TRUE o FALSE
+			//$mysqli = @mysqli_connect("127.0.0.1", "root", "123", "a_prueba");
 			$mysqli = mysqli_connect("127.0.0.1", "root", "123", "a_prueba");
 			
 			//$mysqli = new mysqli("127.0.0.1", "root", "123", "a_prueba");
